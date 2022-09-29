f740dedbc32a720bbfd40b06b03ec8e3f39117fa
Lilranime <112871171+Lilranime@users.noreply.github.com>
2022-09-29 14:17:33 +0100

    Enumerating temperature of water

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Watertemperature myWater = Watertemperature.high;
+        System.out.println("The water temperature is: "+myWater);
+    }
+}
---
+++ src/Watertemperature.java
@@ -0,0 +1,3 @@
+public enum Watertemperature {
+    low, high, medium
+}
